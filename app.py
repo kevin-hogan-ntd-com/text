89e85df2a01fd88ccb6943bfadf818bec042a169
kevin-hogan-ntd-com <kevin.hogan@ntd.com>
2025-08-02 10:46:59 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+# app.py
+from flask import Flask, request, Response
+from twilio.twiml.messaging_response import MessagingResponse
+
+app = Flask(__name__)
+
+@app.route("/sms", methods=['POST'])
+def sms_reply():
+    incoming_msg = request.form.get('Body', '')
+    print("Got SMS:", incoming_msg)
+
+    # Create Twilio XML response
+    resp = MessagingResponse()
+    resp.message("message received")
+    return Response(str(resp), mimetype="application/xml")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=7860)
