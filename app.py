3d72cbb93bfd69d6d52d060801ea5703e1c97972
kevin-hogan-ntd-com <kevin.hogan@ntd.com>
2025-08-02 11:03:06 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
-# app.py
 from flask import Flask, request, Response
 from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
 
-@app.route("/sms", methods=['POST'])
+@app.route("/sms", methods=["POST"])
 def sms_reply():
     incoming_msg = request.form.get('Body', '')
     print("Got SMS:", incoming_msg)
 
-    # Create Twilio XML response
     resp = MessagingResponse()
     resp.message("message received")
+
     return Response(str(resp), mimetype="application/xml")
+
+@app.route("/", methods=["GET"])
+def root():
+    return "OK"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=7860)
